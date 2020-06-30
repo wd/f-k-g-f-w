d2aedfdf48e666a76b58467c74f34f73069f7566
Dong Wang <wd@wdicc.com>
2020-06-30 16:16:52 +0800

    use 3rd party geosite,geoip files

--- router/run.sh
+++ router/run.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/opt/bin/bash
+
+base=$(dirname $0)
+port=12345
+lan='10.10.8.1/24'
+v2ray=$base/bin/v2ray
+config=$base/config.json
 
 log() {
 	echo $(date) "$@"
@@ -9,7 +15,7 @@ start_ip6tables() {
 	ip -f inet6 rule add fwmark 0x01/0x01 table 100
 	ip -f inet6 route add local ::/0 dev lo table 100
 	ip6tables -t mangle -N V2RAY
-	ip6tables -t mangle -A V2RAY -p tcp -j TPROXY --on-port 12345 --tproxy-mark 0x01/0x01
+	ip6tables -t mangle -A V2RAY -p tcp -j TPROXY --on-port $port --tproxy-mark 0x01/0x01
 	ip6tables -t mangle -A PREROUTING -p tcp -j V2RAY
 }
 
@@ -25,11 +31,10 @@ stop_ip6tables() {
 start_iptables() {
 	log "start iptables"
 	iptables -t nat -N V2RAY
-	iptables -t nat -A V2RAY -d 10.10.10.1/24 -j RETURN
-	iptables -t nat -A V2RAY -i enx000ec6d312c3 -p tcp -j RETURN -m mark --mark 0xff
-	iptables -t nat -A V2RAY -i enx000ec6d312c3 -p tcp -j REDIRECT --to-ports 12345
-	iptables -t nat -A V2RAY -o ppp0 -p tcp -j RETURN -m mark --mark 0xff
-	iptables -t nat -A V2RAY -o ppp0 -p tcp -j REDIRECT --to-ports 12345
+	iptables -t nat -A V2RAY -d $lan -j RETURN
+	iptables -t nat -A V2RAY -s 10.10.8.8 -j RETURN # hardcoded
+	iptables -t nat -A V2RAY -p tcp -j RETURN -m mark --mark 0xff
+	iptables -t nat -A V2RAY -p tcp -j REDIRECT --to-ports $port
 	iptables -t nat -A PREROUTING -p tcp -j V2RAY
 	iptables -t nat -A OUTPUT -p tcp -j V2RAY
 }
@@ -45,31 +50,33 @@ stop_iptables() {
 
 start_v2ray() {
 	log "start v2ray"
-	systemctl start v2ray
-	#/usr/bin/v2ray/v2ray -config /etc/v2ray/config.json </dev/null >/dev/null 2>&1 &
+	$v2ray -config $config </dev/null >/dev/null 2>&1 &
 }
 
 stop_v2ray() {
 	log "stop v2ray"
-	systemctl stop v2ray
-	#killall v2ray
+	killall v2ray
 }
 
 check_v2ray() {
-	c=$(ps aux|grep /usr/bin/v2ray/v2ray|grep -v grep|wc -l)
+	c=$(ps|grep $v2ray|grep -v grep|wc -l)
 	if [ "$c" -ne 1 ];then
 		log "v2ray dead: $c"
-		restart
+		stop_v2ray
+		sleep 1
+		start_v2ray
 	else
 		log "v2ray alive: $c"
 	fi
 }
 
 check_iptables() {
-	c=$(iptables -t nat -nL V2RAY |wc -l)
-	if [ "$c" -ne 5 ];then
+	c=$(iptables -t nat -nL V2RAY|grep $port|wc -l)
+	if [ "$c" -ne 1 ];then
 		log "iptables dead: $c"
-		restart
+		stop_iptables
+		sleep 1
+		start_iptables
 	else
 		log "iptables alive: $c"
 	fi
@@ -93,6 +100,18 @@ restart() {
 	start
 }
 
+update() {
+    log "update v2ray"
+    curl -s "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | jq -r '.assets[] | select(.name | endswith("arm64.zip")).browser_download_url' | xargs curl -L -o /tmp/v2ray.zip --url
+	unzip -fo /tmp/v2ray.zip v2ctl v2ray -d $base/bin
+
+	log "update geosite"
+	curl -s -L 'https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat' -o $(dirname $v2ray)/geosite.dat
+
+	log "update geoip"
+	curl -s -L 'https://github.com/Loyalsoldier/geoip/raw/release/geoip.dat' -o $(dirname $v2ray)/geoip.dat
+}
+
 check() {
 	check_v2ray
 	check_iptables
@@ -106,4 +125,6 @@ elif [ "$1" == "check" ];then
 	check
 elif [ "$1" == "restart" ];then
 	restart
+elif [ "$1" == "update" ];then
+    update
 fi
